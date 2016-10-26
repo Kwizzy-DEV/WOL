deeb3012652e04f7d1a2e1c026d90fa456c75ff4
Kwizzy-DEV <kwizzy@outlook.fr>
2016-10-26 12:11:59 +0200

    Fiew modifications

--- src/fr/kwizzy/waroflegions/util/bukkit/schematics/blocks/ChestSchematicBlock.java
+++ src/fr/kwizzy/waroflegions/util/bukkit/schematics/blocks/ChestSchematicBlock.java
@@ -1,8 +1,9 @@
 package fr.kwizzy.waroflegions.util.bukkit.schematics.blocks;
 
+import org.bukkit.inventory.ItemStack;
+
 import fr.kwizzy.waroflegions.util.bukkit.jnbt.CompoundTag;
 import fr.kwizzy.waroflegions.util.bukkit.schematics.SchematicBlock;
-import org.bukkit.inventory.ItemStack;
 
 public class ChestSchematicBlock extends SchematicBlock {
 
@@ -12,9 +13,9 @@ public class ChestSchematicBlock extends SchematicBlock {
 	public ChestSchematicBlock(CompoundTag tag, int x, int y, int z, int id, byte data) {
 		super(x, y, z, id, data);
 		/*
-		 * NbtFactory.NbtList list = nbtCompound.getList("Items", true); items = new ItemStack[27]; for(Object it : list){ if(it instanceof NbtFactory.NbtCompound){ NbtFactory.NbtCompound itm = (NbtFactory.NbtCompound) it; byte slot = itm.getByte("Slot",(byte) 0); if (slot >= 0 && slot < 27) {
-		 * items[slot] = new ItemStack(itm.getInteger("id", 0), itm.getByte("Count", (byte) 0), itm.getShort("Damage", (short) 0)); }
-		 * 
+		 * NbtFactory.NbtList list = nbtCompound.getList("Items", true); items = new ItemStackJson[27]; for(Object it : list){ if(it instanceof NbtFactory.NbtCompound){ NbtFactory.NbtCompound itm = (NbtFactory.NbtCompound) it; byte slot = itm.getByte("Slot",(byte) 0); if (slot >= 0 && slot < 27) {
+		 * items[slot] = new ItemStackJson(itm.getInteger("id", 0), itm.getByte("Count", (byte) 0), itm.getShort("Damage", (short) 0)); }
+		 *
 		 * } }
 		 */
 	}
